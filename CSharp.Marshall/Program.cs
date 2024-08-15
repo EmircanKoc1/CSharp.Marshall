8cb580f302e9fef3269547d2677e28b7f86e659c
Emircan <emircandv787@gmail.com>
2024-08-15 05:02:03 +0300

    Add project files.

---
+++ CSharp.Marshall/Program.cs
@@ -0,0 +1,31 @@
+﻿
+using System;
+
+namespace CSharp.Marshall.ConsoleApp;
+
+
+public static class Program
+{
+
+    public static void Main(string[] args)
+    {
+
+
+        
+
+
+
+
+
+
+    }
+
+
+
+
+
+}
+
+
+
+
